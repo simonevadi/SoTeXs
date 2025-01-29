8fe9eeab3bea7b0072e7fbf461676d635b30b385
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-01-29 14:03:48 +0100

    new simulations

---
+++ simulation/5/run_scripts_select.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+pgrep -f 'simulation_' | xargs kill
+printf "\n"
+printf "\n"
+printf "\n"
+printf "\n"
+printf $py_file 
+python simulation_1200_RP.py
+python simulation_1200_RP.py
+find . -type d -name "round_*" -exec rm -r {} +
+
+python simulation_1200_Flux.py
+python simulation_1200_Flux.py
+find . -type d -name "round_*" -exec rm -r {} +
+
+
