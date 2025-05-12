b7bb78bb19e181efb4d124793b8c890270064072
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-05-12 14:31:22 +0200

    new sim

--- simulation/10/run_scripts_select.sh
+++ simulation/10/run_scripts_select.sh
@@ -4,11 +4,15 @@
 pgrep -f 'simulation_' | xargs kill
 printf "\n"
 printf "\n"
-printf "\n"
-printf "\n"
 # python simulation_1200.py
+# printf "\n"
+# printf "\n"
 # python simulation_1200_07.py
-python simulation_2400_ML.py
+printf "\n"
+printf "\n"
+python simulation_2400_ML_08.py
+printf "\n"
+printf "\n"
 python simulation_2400_ML_07.py
 
 
