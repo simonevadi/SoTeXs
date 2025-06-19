a646710aaa0efffdb963a206c67ff2dc7210cef6
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-06-19 14:46:48 +0200

    simulation_1200

--- simulation/12/run_scripts_select.sh
+++ simulation/12/run_scripts_select.sh
@@ -4,13 +4,19 @@ sudo -v            # Ask for sudo password upfront
 
 
 
-# python simulation_1200_07.py
-# printf "\n"
-# printf "\n"
-# python simulation_2400_ML_07.py
-
-# python simulation_1200_slopes_smart.py
+python simulation_1200_07.py
 git add .
-git commit -m "simulation_1200_slopes_smart"
+git commit -m "simulation_1200"
+git push origin main
+
+
+python simulation_2400_ML_07.py
+git add .
+git commit -m "simulation_2400"
+git push origin main
+
+python simulation_1200_slopes_smart.py
+git add .
+git commit -m "simulation_1200_slopes"
 git push origin main
 # sudo reboot        # Reboot without asking for password again
